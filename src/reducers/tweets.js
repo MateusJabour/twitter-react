62f24860d2c3734c68636d27536e94390d14cb31
Mateus Jabour <mjabourbp@gmail.com>
2018-02-28 11:17:55 -0500

    Removes import and updates tweets reducer

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,13 +1,22 @@
 function tweets(state = [], action) {
   switch (action.type) {
-    case "CREATE_TWEET":
-      return [...state, action.tweet];
+    case "TWEET_CREATION_SUCCESS":
+      return [ action.tweet, ...state];
+    case "TWEET_DELETION_SUCCESS":
+      const tweetIndex = state.findIndex((tweet) => tweet.id === action.id);
+      if (tweetIndex >= 0) {
+        return state.length ? [
+          ...state.slice(0, tweetIndex),
+          ...state.slice(tweetIndex + 1),
+        ] : [];
+      } else {
+        return state;
+      }
     case "RECEIVE_TWEETS":
       return action.tweets;
     default:
-
+      return state;
   }
-  return state;
 }
 
 export default tweets;

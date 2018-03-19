7978c9cc80ffe01778aff345540020e0346d37fa
Mateus Jabour <mjabourbp@gmail.com>
2018-03-19 16:40:01 -0400

    Changes routes and add helper function

--- src/helpers.js
+++ src/helpers.js
@@ -15,3 +15,26 @@ export function parseJwt (token) {
   const base64 = base64Url.replace('-', '+').replace('_', '/');
   return JSON.parse(window.atob(base64));
 };
+
+export function difFromCreationDate(date) {
+  const today = Date.now() / 1000;
+  const photoDate = new Date(date).getTime() / 1000;
+
+  const difInSec = today - photoDate;
+  if (difInSec > 60) {
+    const difInMin = difInSec / 60;
+    if (difInMin > 60) {
+      const difInHour = difInMin / 60;
+      if (difInHour > 24) {
+        const difInDay = difInHour / 24;
+        return Math.floor(difInDay) + 'd';
+      } else {
+        return Math.floor(difInHour) + 'h';
+      }
+    } else {
+      return Math.floor(difInMin) + 'm';
+    }
+  } else {
+    return Math.floor(difInSec) + 's';
+  }
+}
--- src/twitter.js
+++ src/twitter.js
@@ -8,6 +8,7 @@ import Timeline from './components/Timeline';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import SingleUser from './components/SingleUser';
+import Users from './components/Users';
 
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
@@ -16,10 +17,11 @@ import store, { history } from './store';
 const router = (
   <Provider store={store}>
     <Router history={history}>
+      <Route path="/login" component={Login}></Route>
+      <Route path="/signup" component={Signup}></Route>
       <Route path="/" component={App}>
         <IndexRoute component={Timeline}/>
-        <Route path="/login" component={Login}></Route>
-        <Route path="/signup" component={Signup}></Route>
+        <Route path="/users" component={Users}></Route>
         <Route path="/user/:id" component={SingleUser}></Route>
       </Route>
     </Router>
